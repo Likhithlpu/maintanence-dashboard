93a72b59d2c120a2cafc4bb8f36634edb7febd11
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2023-08-18 13:15:10 +0530

    Added 8 panes to the 3rd pane

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,37 @@ import React from 'react';
 import './HomePage.css'; // Import the corresponding CSS file for styling
 import FeedbackForm from '../components/FeedbackForm/FeedbackForm';
 
-
 const HomePage = () => {
   return (
     <div className="home-page">
       <div className="section">
-        <div className="split-pane"><iframe src="https://smartcitylivinglab.iiit.ac.in/grafana/d-solo/kyLuJXQ7z/summary-view?kiosk=&autofitpanels=&orgId=1&from=1690610926019&to=1692338926019&theme=dark&panelId=16" width="350" height="300" frameborder="0"></iframe></div>
+        <div className="split-pane">
+          <iframe src="https://smartcitylivinglab.iiit.ac.in/grafana/d-solo/kyLuJXQ7z/summary-view?kiosk=&autofitpanels=&orgId=1&from=1690610926019&to=1692338926019&theme=dark&panelId=16" width="450" height="300" frameborder="0"></iframe>
+        </div>
         <div className="split-pane">Pane 2</div>
       </div>
-      <div className="section single-pane">Pane 3</div>
       <div className="section">
-      <FeedbackForm />
+        <div className="split-pane">
+          <div className="row">
+            <div className="column">Pane 3.1</div>
+            <div className="column">Pane 3.2</div>
+          </div>
+          <div className="row">
+            <div className="column">Pane 3.3</div>
+            <div className="column">Pane 3.4</div>
+          </div>
+          <div className="row">
+            <div className="column">Pane 3.5</div>
+            <div className="column">Pane 3.6</div>
+          </div>
+          <div className="row">
+            <div className="column">Pane 3.7</div>
+            <div className="column">Pane 3.8</div>
+          </div>
+        </div>
+      </div>
+      <div className="section">
+        <FeedbackForm />
       </div>
     </div>
   );
