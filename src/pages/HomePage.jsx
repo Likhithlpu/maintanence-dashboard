37a6b3ef62ffcf47d6dfb84235f2f61ce18a6ea0
Bala Likhith Kanigolla <55309319+Likhithlpu@users.noreply.github.com>
2023-08-18 16:34:45 +0530

    Added Stacked Bar Chart

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomePage.css'; // Import the corresponding CSS file for styling
 import FeedbackForm from '../components/FeedbackForm/FeedbackForm';
+import StackedColumn100Chart from '../components/charts/Stacked Column 100 Chart';
 
 const HomePage = () => {
   return (
@@ -9,7 +10,7 @@ const HomePage = () => {
         <div className="split-pane">
           <iframe src="https://smartcitylivinglab.iiit.ac.in/grafana/d-solo/kyLuJXQ7z/summary-view?kiosk=&autofitpanels=&orgId=1&from=1690610926019&to=1692338926019&theme=dark&panelId=16" width="450" height="300" frameborder="0"></iframe>
         </div>
-        <div className="split-pane">Pane 2</div>
+        <div className="split-pane"><StackedColumn100Chart /></div>
       </div>
       <div className="section">
         <div className="split-pane">
