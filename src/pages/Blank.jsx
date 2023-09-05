cf65e6721afd6f15c27c73e72de5800a12adf664
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-09-06 01:02:49 +0530

    Added Panes in the Air Quality

--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -1,5 +1,7 @@
 const Air = () => {
-    return <div>This is Air Quality page</div>;
+    return (    <div className="home-page">
+    <iframe src="https://smartcitylivinglab.iiit.ac.in/grafana/d/f81d3e9d-84c6-43e5-a1e6-f4ba76bbcf6b/page-dashboard?&kiosk&autofitpanels&orgId=1" width="100%" height="100%" frameborder="0"></iframe>
+</div>);
 };
 
 const Default = () => {
