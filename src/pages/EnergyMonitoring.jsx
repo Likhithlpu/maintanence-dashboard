cb23a476b05bf9b0e10eb390bd6126df2d7f6451
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-10-02 19:06:25 +0530

    Updated EM and Solar Vertical

--- src/pages/EnergyMonitoring.jsx
+++ src/pages/EnergyMonitoring.jsx
@@ -1,18 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Energy = () => {
-  const items = [
-    "EM-NC-VN01-00",
-    "EM-NC-VN01-01",
-    "EM-NC-VN92-00",
-    "EM-NC-VN93-00",
-    // Add more items as needed
-  ];
+  const items = [];
 
   const sliderRef = useRef(null);
+  const iframeHeight = '80vh'; // Adjust the height as needed
 
   const settings = {
     dots: true,
@@ -29,10 +24,30 @@ const Energy = () => {
     }
   };
 
+  useEffect(() => {
+    // Adjust iframe height when the window is resized
+    const handleResize = () => {
+      if (sliderRef.current) {
+        const iframe = sliderRef.current.innerSlider.list.querySelector('iframe');
+        if (iframe) {
+          iframe.style.height = iframeHeight;
+        }
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const iframeContainerHeight = '780px'; 
+
   return (
-    <div style={{ maxWidth: '95%', margin: '0 auto' }}>
+    <div style={{ maxWidth: '95%', margin: '0 auto'}}>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }}>
-        <h1 style={{color: '#fff'}}>Energy Monitoring</h1>
+        <h1 style={{ color: '#fff', padding: '20px' }}>Energy Monitoring</h1>
         <select
           onChange={(e) => handleSelectItem(e.target.value)}
           style={{
@@ -54,8 +69,9 @@ const Energy = () => {
       </div>
       <Slider ref={sliderRef} {...settings}>
         {items.map((item, index) => (
-          <div key={index} style={{ textAlign: 'center', padding: '20px' }}>
-            <h3>{item}</h3>
+          <div key={index} style={{ textAlign: 'center', padding: '20px'}}>
+            {/* <h3>{item}</h3> */}
+            <iframe src={`https://smartcityresearch.iiit.ac.in/grafana/d/f81d3e9d-84c6-43e5-a1e6-f4ba76b-sr-ac/smart-room-air-conditioner?kiosk&var-nodeid=${item}&orgId=1`} width="100%" height='600px' style={{height: iframeContainerHeight}}></iframe>
           </div>
         ))}
       </Slider>
--- src/pages/SolarMonitoring.jsx
+++ src/pages/SolarMonitoring.jsx
@@ -1,5 +1,82 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const Solar = () => {
-    return <div>This is Solar page</div>;
+  const items = [];
+
+  const sliderRef = useRef(null);
+  const iframeHeight = '80vh'; // Adjust the height as needed
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const handleSelectItem = (item) => {
+    const index = items.indexOf(item);
+    if (sliderRef.current && index !== -1) {
+      sliderRef.current.slickGoTo(index);
+    }
+  };
+
+  useEffect(() => {
+    // Adjust iframe height when the window is resized
+    const handleResize = () => {
+      if (sliderRef.current) {
+        const iframe = sliderRef.current.innerSlider.list.querySelector('iframe');
+        if (iframe) {
+          iframe.style.height = iframeHeight;
+        }
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const iframeContainerHeight = '780px'; 
+
+  return (
+    <div style={{ maxWidth: '95%', margin: '0 auto'}}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }}>
+        <h1 style={{ color: '#fff', padding: '20px' }}>Solar Monitoring</h1>
+        <select
+          onChange={(e) => handleSelectItem(e.target.value)}
+          style={{
+            padding: '8px',
+            border: 'none',
+            borderRadius: '4px',
+            backgroundColor: '#333', // Dark background color
+            color: '#fff', // Text color
+            cursor: 'pointer',
+          }}
+        >
+          <option value="">Select an item</option>
+          {items.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Slider ref={sliderRef} {...settings}>
+        {items.map((item, index) => (
+          <div key={index} style={{ textAlign: 'center', padding: '20px'}}>
+            {/* <h3>{item}</h3> */}
+            <iframe src={`https://smartcityresearch.iiit.ac.in/grafana/d/f81d3e9d-84c6-43e5-a1e6-f4ba76bbcf6b-sl/solar-monitoring?kiosk&var-nodeid=${item}&orgId=1`} width="100%" height='600px' style={{height: iframeContainerHeight}}></iframe>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default Solar;
