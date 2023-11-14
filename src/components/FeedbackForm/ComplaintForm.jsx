4729418a95099bf606a2bb0f50d0efc0296afdea
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-14 21:22:01 +0530

    Modified Complaint/Feedback Form

--- src/components/FeedbackForm/ComplaintForm.jsx
+++ src/components/FeedbackForm/ComplaintForm.jsx
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { FiUser, FiMail, FiPhone, FiMessageSquare, FiBox } from 'react-icons/fi';
-import { Card, Form, Alert, InputGroup, Row, Col, Button } from 'react-bootstrap';
-
+import { Card, Form, InputGroup, Row, Col, Button } from 'react-bootstrap';
 import { BsLayoutTextSidebarReverse } from 'react-icons/bs';
 import './ComplaintForm.css';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5002',
   baseURL: 'https://smartcitylivinglab.iiit.ac.in/maintenance-dashboard-api',
 });
 
 const ComplaintForm = () => {
+  const [stage, setStage] = useState(1);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [contactNumber, setContactNumber] = useState('');
@@ -70,6 +69,210 @@ const ComplaintForm = () => {
     }
   };
 
+  const renderForm = () => {
+    switch (stage) {
+      case 1:
+        return (
+          <div>
+            <h2 className="form-title">Basic Information</h2>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    <FiUser /> Name:
+                  </Form.Label>
+                  <InputGroup>
+                    <input
+                      type="text"
+                      placeholder="E.g. Jon Snow"
+                      value={name}
+                      onChange={handleNameChange}
+                      className="form-input"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    <FiMail /> Email:
+                  </Form.Label>
+                  <InputGroup>
+                    <input
+                      type="email"
+                      placeholder="E.g. abc@gmail.com"
+                      value={email}
+                      onChange={handleEmailChange}
+                      className="form-input"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    <FiPhone /> Phone:
+                  </Form.Label>
+                  <InputGroup>
+                    <input
+                      type="text"
+                      placeholder="E.g. 9999999999"
+                      value={contactNumber}
+                      onChange={handleContactNumberChange}
+                      className="form-input"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    <FiMessageSquare /> Type of Request
+                  </Form.Label>
+                  <InputGroup>
+                    <Form.Select
+                      value={feedbackType}
+                      onChange={handleFeedbackTypeChange}
+                      className="form-input"
+                      required
+                    >
+                      <option value="complaint">Complaint</option>
+                      <option value="feedback">Feedback</option>
+                    </Form.Select>
+                  </InputGroup>
+                </Form.Group>
+              </Col>
+            </Row>
+            <button
+              type="button"
+              className="form-submit-button"
+              onClick={() => setStage(2)}
+            >
+              Next
+            </button>
+          </div>
+        );
+
+      case 2:
+        return (
+          <div>
+            <h2 className="form-title">
+              {feedbackType === 'feedback' ? 'Feedback' : 'Complaint'} Form
+            </h2>
+            {feedbackType === 'feedback' ? (
+              <Row>
+                <Col>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <FiMessageSquare /> Feedback:
+                    </Form.Label>
+                    <InputGroup>
+                      <textarea
+                        value={complaint}
+                        onChange={handleComplaintChange}
+                        placeholder="Enter your feedback..."
+                        className="form-input"
+                        required
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                </Col>
+              </Row>
+            ) : (
+              <div>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3">
+                      <Form.Label>
+                        <FiBox /> Vertical Name:
+                      </Form.Label>
+                      <InputGroup>
+                        <Form.Select
+                          value={Vertical}
+                          onChange={handleVerticalChange}
+                          className="form-input"
+                          required
+                        >
+                          <option value="AQ">Air Quality</option>
+                          <option value="SL">Solar Monitoring</option>
+                          <option value="SL-Energy Meter">Solar Energy Meter</option>
+                          <option value="WM-WD">Water Distribution</option>
+                          <option value="WM-WF-Shenitech">Water Flow Shenitech</option>
+                          <option value="WM-WF-Retrofit">Water Flow Retrofit</option>
+                          <option value="WM-WF-Kristnam">Water Flow Kristnam</option>
+                          <option value="SR-AC">Smart Room Air Conditioner</option>
+                          <option value="SR-AQ">Smart Room Air Quality</option>
+                          <option value="SR-EM">Smart Room Energy Meter</option>
+                          <option value="SR-OC">Smart Room Occupancy</option>
+                          <option value="CM">Crowd Monitoring</option>
+                          <option value="WN-FAN10FSK">Wi-SUN FAN10FSK</option>
+                          <option value="WN-FAN11FSK">Wi-SUN FAN11FSK</option>
+                          <option value="WN-FAN11OFDM">Wi-SUN FAN11OFDM</option>
+                          {/* Add more options as needed */}
+                        </Form.Select>
+                      </InputGroup>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3">
+                      <Form.Label>
+                        <BsLayoutTextSidebarReverse /> Node ID:
+                      </Form.Label>
+                      <InputGroup>
+                        <input
+                          type="text"
+                          placeholder="E.g. AQ-AN00-00"
+                          value={nodeId}
+                          onChange={handleNodeidChange}
+                          className="form-input"
+                          required
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3">
+                      <Form.Label>
+                        <FiMessageSquare /> Complaint:
+                      </Form.Label>
+                      <InputGroup>
+                        <textarea
+                          value={complaint}
+                          onChange={handleComplaintChange}
+                          placeholder="Enter your complaint..."
+                          className="form-input"
+                          required
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </div>
+            )}
+            <button type="submit" className="form-submit-button">
+              Submit Review
+            </button>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
   if (isSubmitted) {
     return (
       <div className="confirmation-message">
@@ -98,163 +301,7 @@ const ComplaintForm = () => {
     );
   }
 
-  return (
-    <form onSubmit={handleSubmit} className="complaint-form">
-      <h2 className="form-title">Feedback Form</h2>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiUser /> Name:
-            </Form.Label>
-            <InputGroup>
-              <input
-                type="text"
-                placeholder="E.g. Jon Snow"
-                value={name}
-                onChange={handleNameChange}
-                className="form-input"
-                required
-              />
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiMail /> Email:
-            </Form.Label>
-            <InputGroup>
-              <input
-                type="email"
-                placeholder="E.g. abc@gmail.com"
-                value={email}
-                onChange={handleEmailChange}
-                className="form-input"
-                required
-              />
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiPhone /> Phone:
-            </Form.Label>
-            <InputGroup>
-              <input
-                type="text"
-                placeholder="E.g. 9999999999"
-                value={contactNumber}
-                onChange={handleContactNumberChange}
-                className="form-input"
-                required
-              />
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiBox /> Vertical Name:
-            </Form.Label>
-            <InputGroup>
-              <Form.Select
-                type="text"
-                placeholder="E.g. AQ"
-                value={Vertical}
-                onChange={handleVerticalChange}
-                className="form-input"
-                required
-              >
-                <option value="AQ">Air Quality</option>
-                <option value="SL">Solar Monitoring</option>
-                <option value="SL-Energy Meter">Solar Energy Meter</option>
-                <option value="WM-WD">Water Distribution</option>
-                <option value="WM-WF-Shenitech">Water Flow Shenitech</option>
-                <option value="WM-WF-Retrofit">Water Flow Retrofit</option>
-                <option value="WM-WF-Kristnam">Water Flow Kristnam</option>
-                <option value="SR-AC">Smart Room Air Conditioner</option>
-                <option value="SR-AQ">Smart Room Air Quality</option>
-                <option value="SR-EM">Smart Room Energy Meter</option>
-                <option value="SR-OC">Smart Room Occupancy</option>
-                <option value="CM">Crowd Monitoring</option>
-                <option value="WN-FAN10FSK">Wi-SUN FAN10FSK</option>
-                <option value="WN-FAN11FSK">Wi-SUN FAN11FSK</option>
-                <option value="WN-FAN11OFDM">Wi-SUN FAN11OFDM</option>
-              </Form.Select>
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <BsLayoutTextSidebarReverse /> Node ID:
-            </Form.Label>
-            <InputGroup>
-              <input
-                type="text"
-                placeholder="E.g. AQ-AN00-00"
-                value={nodeId}
-                onChange={handleNodeidChange}
-                className="form-input"
-                required
-              />
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiMessageSquare /> Type of Request
-            </Form.Label>
-            <InputGroup>
-              <Form.Select
-                value={feedbackType}
-                onChange={handleFeedbackTypeChange}
-                className="form-input"
-                required
-              >
-                <option value="complaint">Complaint</option>
-                <option value="feedback">Feedback</option>
-              </Form.Select>
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <FiMessageSquare /> Complaint/Feedback:
-            </Form.Label>
-            <InputGroup>
-              <textarea
-                value={complaint}
-                onChange={handleComplaintChange}
-                placeholder="Enter your complaint..."
-                className="form-input"
-                required
-              />
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-      <button type="submit" className="form-submit-button">
-        Submit Review
-      </button>
-    </form>
-  );
+  return <form onSubmit={handleSubmit}>{renderForm()}</form>;
 };
 
 export default ComplaintForm;
