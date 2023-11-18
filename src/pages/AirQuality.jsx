517c9a0c259eafa65ee62e1b6a066f9a96c58382
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-18 18:27:01 +0530

    Updated URL for AQ

--- src/pages/AirQuality.jsx
+++ src/pages/AirQuality.jsx
@@ -102,11 +102,12 @@ const Air = () => {
 
   const lazyLoadIframe = (item, index) => {
     const datatable='data_airqualitydatalatest'
+    const vertical='AQ'
     if (status === 'Inactive') {
       return (
         <iframe
           title="Air Quality Analytics"
-          src={`https://smartcitylivinglab.iiit.ac.in/grafana/d/ca895582-79c6-4b57-b95c-17135ff82b44/inactive-dashboard?kiosk&var-nodeid=${item}&refresh=5s&var-datatable=${datatable}&orgId=1`}
+          src={`https://smartcitylivinglab.iiit.ac.in/grafana/d/ca895582-79c6-4b57-b95c-17135ff82b44/inactive-dashboard?kiosk&var-nodeid=${item}&var-vertical=${vertical}&refresh=5s&var-datatable=${datatable}&orgId=1`}
           width="100%"
           height={iframeContainerHeight}
           style={{ height: iframeContainerHeight }}
